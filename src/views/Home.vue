3bfeb24d21735dcdb365432ec24ec6233b82c763
tonyshu168 <493397276@qq.com>
2019-08-10 16:12:09 +0800

    initial project

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" @callback="test" @testFunc="test"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    test() {
+      console.log('test');
+    },
+  }
+};
+</script>
